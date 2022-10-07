3b38b89ecd78eb4f525e06bbd941d16c43c1a772
unknown <bi.madhubabu@gmail.com>
2022-10-07 17:57:23 -0400

    Django_Demo Inital Commit all works well

---
+++ apps/common/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from django.views.generic import TemplateView, CreateView
+
+from .forms import SignUpForm
+
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'common/home.html'
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('home')
+    template_name = 'common/register.html'
